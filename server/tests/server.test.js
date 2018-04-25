8a52e06f396cc5b09b16f74fb77a9052ff0baefb
Moore <mooredh@gmail.com>
2018-04-25 21:12:41 +0100

    Tested the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -113,3 +113,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 })
+
+describe('DELETE /todos/:id', () => {
+    it('shoulld remove a todo', done => {
+        let id = todos[0]._id.toHexString();
+        request(app)
+            .delete(`/todos/${id}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo._id).toBe(id);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(id).then((todo) => {
+                    expect(todo).toBeFalsy();
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', done => {
+        let id = new ObjectID();
+
+        request(app)
+            .delete(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .end(done);
+    })
+
+    it("should return a 404 if for non-object ids", done => {
+        request(app)
+            .delete(`/todos/123`)
+            .expect(404)
+            .end(done);
+    });
+})
